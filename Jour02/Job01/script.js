6c2b884aa7464a7d825de9ef0aea6a9cbafb9647
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-03-12 08:24:11 +0100

    Create script.js
    script job01

---
+++ Jour02/Job01/script.js
@@ -0,0 +1,6 @@
+function citation() {
+    let texteCitation = document.getElementById("citation").textContent;
+    console.log(texteCitation);
+}
+
+document.getElementById("button").addEventListener("click", citation);
